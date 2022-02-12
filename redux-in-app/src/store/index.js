6cc0c98d69e45cafaa6883fb0e5e902fa77c4335
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-02-12 01:58:24 -0700

    changed if statements for switch statement

--- redux-in-app/src/store/index.js
+++ redux-in-app/src/store/index.js
@@ -3,35 +3,30 @@ import { createStore } from "redux";
 const initialState = { counter: 0, showCounter: true };
 
 const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+        showCounter: state.showCounter,
+      };
+    case "increase":
+      return {
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };
+    case "toggle":
+      return {
+        showCounter: !state.showCounter,
+        counter: state.counter,
+      };
+    default:
+      return state;
   }
-
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
 };
 
 const store = createStore(counterReducer);
